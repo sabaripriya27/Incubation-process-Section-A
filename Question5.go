5aaaaf1c3dab4521084c36ac2d310cfbfb577f64
sabaripriya27 <96071701+sabaripriya27@users.noreply.github.com>
2022-04-10 10:30:10 +0530

    Update Question5.go

--- Question5.go
+++ Question5.go
@@ -5,13 +5,14 @@ import "fmt"
 func main() {
 	//var i int
 	var a string = "abcdef"
-	j := len(a)
+	n:= len(a)
 
-	for i := 0; i < j; i++ {
-		if (a[i+1] - a[i]) == (a[j] - a[j-1]) {
+	for i := 0; i < n; i++ {
+		if (a[i+1] - a[i]) == (a[n] - a[n-1]) {
 			fmt.Println("Given string is equal difference ")
 		} else {
 			fmt.Println("Given string is unequal difference ")
 		}
 	}
+}
 }
